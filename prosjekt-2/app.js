f593c27c77177fb988621481b0884601d4e69b46
lsnellingen <larssnellingen@hotmail.com>
2017-09-07 16:35:42 +0200

    Laget en testside med express

--- prosjekt-2/app.js
+++ prosjekt-2/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var test = require('./routes/test')
 
 var app = express();
 
@@ -16,6 +17,8 @@ var cons = require('consolidate');
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/test', test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
